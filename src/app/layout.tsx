4a6f737790ddb5bd04054399963e22b6f80fb5d6
listerineh <sebask8er.alvarez@gmail.com>
2025-07-06 14:14:07 -0500

    feat: PWA improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,8 +57,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <link rel="apple-touch-icon" href="/icon.svg"></link>
-        <meta name="theme-color" content="#2cc8a9" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
