588e3d76f0a8c07595a78b249cef1a85d90872e8
listerineh <sebask8er.alvarez@gmail.com>
2025-07-06 14:17:48 -0500

    feat: adding custom icons to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
