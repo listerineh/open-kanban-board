e34937ccd2da0f709d781d76e07977b1f637d98f
listerineh <sebask8er.alvarez@gmail.com>
2025-07-28 22:03:51 -0500

    fix: skeletons design

--- src/components/common/skeletons.tsx
+++ src/components/common/skeletons.tsx
@@ -28,11 +28,15 @@ export function DashboardSkeleton() {
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {Array.from({ length: 3 }).map((_, i) => (
               <Card key={i} className="flex flex-col">
-                <CardHeader>
+                <CardHeader className="pb-4">
                   <Skeleton className="h-6 w-3/4 mb-2" />
                   <Skeleton className="h-4 w-1/2" />
                 </CardHeader>
-                <CardContent className="flex-grow flex items-end">
+                <CardContent className="flex-grow space-y-2">
+                  <Skeleton className="h-4 w-full" />
+                  <Skeleton className="h-4 w-5/6" />
+                </CardContent>
+                <CardContent className="flex items-end pt-4">
                   <Skeleton className="h-4 w-1/3" />
                 </CardContent>
               </Card>
